633cb76641e3de9da46cea96327bcea920a108de
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2023-03-28 13:34:44 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,21 @@ videoList.forEach(vid =>{
       document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
    };
 });
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
